9f72684774364144d679fa32ba2bbe7ba804864e
Travis Roach <travis.roach24@gmail.com>
2018-09-05 09:04:10 -0600

    add client to git repo

---
+++ client/src/plugins/vuetify.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import {
+  Vuetify,
+  VApp,
+  VNavigationDrawer,
+  VFooter,
+  VList,
+  VBtn,
+  VIcon,
+  VGrid,
+  VToolbar,
+  transitions
+} from 'vuetify'
+import 'vuetify/src/stylus/app.styl'
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VNavigationDrawer,
+    VFooter,
+    VList,
+    VBtn,
+    VIcon,
+    VGrid,
+    VToolbar,
+    transitions
+  },
+  theme: {
+    primary: '#092242',
+    secondary: '#E85C2F',
+    accent: '#FFFFFF',
+    error: '#FF5252',
+    info: '#FFFFFF',
+    success: '#4CAF50',
+    warning: '#FFC107'
+  },
+})
