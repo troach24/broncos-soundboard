18d428c9e92e8b710735ec76610339e8b5d28d8f
Travis Roach <travis.roach24@gmail.com>
2018-09-05 11:28:05 -0600

    GET/POST sound bite objects to API

---
+++ server/db/sounds.js
@@ -0,0 +1,32 @@
+const Joi = require('joi');
+const db = require('./connection');
+
+const schema = Joi.object().keys({
+  name: Joi.string().alphanum().required(),
+  soundUrl: Joi.string().uri({
+    scheme: [
+      /https?/
+    ]
+  })
+})
+
+const sounds = db.get('sounds');
+
+function getAll() {
+  return sounds.find();
+}
+
+function createSound(sound) {
+  const result = Joi.validate(sound, schema);
+  if (result.error == null) {
+    sound.created = new Date();
+    return sounds.insert(sound);
+  } else {
+    return Promise.reject(result.error);
+  }
+}
+
+module.exports = {
+  getAll,
+  createSound
+};
--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
+const sounds = require('./db/sounds');
+
 const app = express();
 
 app.use(morgan('tiny'));
@@ -13,7 +15,23 @@ app.get('/', (req, res) => {
   res.json({
     message: '🔷🔶 Denver Broncos sound bite API 🐴'
   });
+});
+
+app.get('/sounds', (req, res) => {
+  sounds.getAll().then((sounds) => {
+    res.json(sounds);
+  });
 })
+
+app.post('/sounds', (req, res) => {
+  console.log(req.body);
+  sounds.createSound(req.body).then((sound) => {
+    res.json(sound);
+  }).catch((error) => {
+    res.status(500);
+    res.json(error);
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
