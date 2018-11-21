9e070374f512364889a4f7b1a14fd8823be9bd3f
Travis Roach <travis.roach24@gmail.com>
2018-11-20 22:50:56 -0700

    show edit mode chip only on /admin

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -8,7 +8,7 @@
         <v-toolbar-title v-text="title"></v-toolbar-title>
       </router-link>
       <v-spacer></v-spacer>
-      <v-chip outline color="#FB4F14">Toggle Edit Mode</v-chip>
+      <v-chip v-if="$route.name == 'admin'" outline color="#FB4F14">Toggle Edit Mode</v-chip>
       <v-tooltip bottom>
         <v-icon
           v-if="$route.name !== 'admin'"
